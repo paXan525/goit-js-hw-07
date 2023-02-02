75107fb0e1121519cca636c8c4ccc5183d5865ed
Pavlo Maslo <maslo525@gmail.com>
2023-02-02 23:09:49 +0200

    Додає готові ДЗ

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this 
 
 console.log(galleryItems);
+
+const imagesGallery = document.querySelector('.gallery');
+
+const listImages = galleryItems.map(({ preview, original, description }) => 
+  `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt='${description}'
+    />
+  </a>
+</div>`
+).join('');
+
+imagesGallery.insertAdjacentHTML('beforeend', listImages);
+
+imagesGallery.addEventListener('click', openImage)
+
+function openImage(event) {
+  event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+  const bigImage = event.target.dataset.source;
+  const modalGallery = basicLightbox.create(
+    `<img src="${bigImage}" width="800" height="800" >`,
+
+    {
+      onShow: modalGallery => { window.addEventListener('keydown', onEscKeyPress) },
+      onClose: modalGallery => { window.removeEventListener('keydown', onEscKeyPress) },
+    }
+  );
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape' && basicLightbox.visible()) {
+      modalGallery.close();
+    }
+  }
+  modalGallery.show();
+};
